fdcb1388e80fc2fe5760158ee2226e8de424dd7d
marco <odri.fo@gmail.com>
2020-11-04 21:19:00 +0100

    finito

--- js/main.js
+++ js/main.js
@@ -1,65 +1,60 @@
 $(document).ready(function(){
 
 // CLASS COST
+
   var hoverCost = $('#cost')
   hoverCost.mouseenter( function() {
-      $('.cost').slideDown();
+    var closeEl = $('.product , .mission , .help , .lang');
+    $('.cost').slideDown();
+    closeEl.hide();
   })
   $('.cost').mouseleave( function() {
-      $('.cost').slideUp();
-  })
-  hoverCost.mouseleave( function() {
-      $('.cost').slideUp();
+    $('.cost').toggle()
+
   })
 
 // CLASS PRODUCT
+
   var hoverProduct = $('#product')
   hoverProduct.mouseenter( function() {
+      var closeEl = $('.cost , .mission , .help , .lang');
       $('.product').slideDown();
+      closeEl.hide();
   })
   $('.product').mouseleave( function() {
-      $('.product').slideUp();
-  })
-  hoverProduct.mouseleave( function() {
       $('.product').slideUp();
   })
 
 // CLASS MISSION
   var hoverMission = $('#mission')
   hoverMission.mouseenter( function() {
+    var closeEl = $('.cost , .product , .help , .lang');
       $('.mission').slideDown();
+        closeEl.hide();
   })
   $('.mission').mouseleave( function() {
       $('.mission').slideUp();
   })
-  hoverMission.mouseleave( function() {
-      $('.mission').slideUp();
-  })
+
 
 // CLASS HELP
   var hoverHelp = $('#help')
   hoverHelp.mouseenter( function() {
+    var closeEl = $('.cost , .mission , .product , .lang');
       $('.help').slideDown();
+      closeEl.hide();
   })
   $('.help').mouseleave( function() {
       $('.help').slideUp();
   })
-  hoverHelp.mouseleave( function() {
-      $('.help').slideUp();
-  })
+
 
   // CLASS lang
   var clickLang = $('#lang')
   clickLang.click( function() {
+    var closeEl = $('.cost , .mission , .product , .help');
+    closeEl.hide();
     $('.lang').toggle();
     $('#lang > i').toggleClass('rotate');
   })
 })
-// $( "li" ).hover(
-//   function() {
-//     $( this ).append( $( "<span> ***</span>" ) );
-//   }, function() {
-//     $( this ).find( "span" ).last().remove();
-//   }
-// );
-// $( selector ).hover( handlerIn, handlerOut )
